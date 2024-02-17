87164e6db9b1521f43eeacb78a95302d1d77d302
MikeYoulend <mikeyoulend@gmail.com>
2024-02-17 20:11:51 +0100

    Parent position

--- Assets/CoordinateLabeler.cs
+++ Assets/CoordinateLabeler.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using TMPro;
+
 
 [ExecuteAlways]
 public class CoordinateLabeler : MonoBehaviour
 {
     TextMeshPro label;
+    //Qui con seconda parte stiamo inizializzando coordinate con i valori predefiniti quindi Vector2( x = 0 & y = 0)
+    Vector2Int coordinates = new Vector2Int(); 
 
     void Awake() //Awake  è letteralmente la prima cosa che viene fatta
     {
@@ -16,9 +18,20 @@ public class CoordinateLabeler : MonoBehaviour
 
     void Update()
     {
-        if(!Application.isPlaying)
+        //Se non abbiamo schiacciato su Play
+        if(!Application.isPlaying) 
         {
-
+            DisplayCoordinates();
         }
+    }
+
+    void DisplayCoordinates()
+    {
+        //Cannot implicitly convert type 'float' to 'int'.
+        //per risolvere questo problema usiamo Mathf.RoundToInt
+        //si usa parent cosi il GameObject figlio prenderà sempre la posizione del padre
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x); 
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z);
+        label.text = coordinates.x + "," + coordinates.y;
     }
 }

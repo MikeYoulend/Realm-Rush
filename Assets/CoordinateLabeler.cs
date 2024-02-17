ffd9dcabbdb36e840435a47f64348f090c23613f
MikeYoulend <mikeyoulend@gmail.com>
2024-02-17 20:42:09 +0100

    Grid name e Hierarchy Name

--- Assets/CoordinateLabeler.cs
+++ Assets/CoordinateLabeler.cs
@@ -14,6 +14,8 @@ public class CoordinateLabeler : MonoBehaviour
     void Awake() //Awake  è letteralmente la prima cosa che viene fatta
     {
         label = GetComponent<TextMeshPro>();
+        //lo richiamiamo anche qui cosi quando premiamo play non crasha 
+        DisplayCoordinates();
     }
 
     void Update()
@@ -22,6 +24,7 @@ public class CoordinateLabeler : MonoBehaviour
         if(!Application.isPlaying) 
         {
             DisplayCoordinates();
+            UpdateObjectName();
         }
     }
 
@@ -30,8 +33,17 @@ public class CoordinateLabeler : MonoBehaviour
         //Cannot implicitly convert type 'float' to 'int'.
         //per risolvere questo problema usiamo Mathf.RoundToInt
         //si usa parent cosi il GameObject figlio prenderà sempre la posizione del padre
-        coordinates.x = Mathf.RoundToInt(transform.parent.position.x); 
-        coordinates.y = Mathf.RoundToInt(transform.parent.position.z);
+        //UnityEditor.EditorSnapSettings.move.x cioò significa....
+        //Dividi la posizione per il movimento che hai impostasto su Unity in questo caso 10 
+        //Quindi in posizione 10,0 diventerà 1,0
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x); 
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
         label.text = coordinates.x + "," + coordinates.y;
+    }
+
+    void UpdateObjectName()
+    {
+        //Nella Hierarchy dai al nome le cordinate 
+        transform.parent.name = coordinates.ToString();
     }
 }

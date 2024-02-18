0d1aea504874f14a4064e25b745497a8814bbf9f
MikeYoulend <mikeyoulend@gmail.com>
2024-02-18 18:42:55 +0100

    GameObject Movement

--- Assets/Script/EnemyMover.cs
+++ Assets/Script/EnemyMover.cs
@@ -5,17 +5,38 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
+   
     // Start is called before the first frame update
     void Start()
-    {
-        PrintWaypointName();
+    {  
+        
+        StartCoroutine(FollowPath());
+        
+        //InvokeRepeating("NomeDelMetodo", tempoDiRitardo, Intervallo)
+        //InvokeRepeating("PrintWaypointName", 0, 1f);
+        
     }
 
-    void PrintWaypointName()
+
+  //IEnumerator in C# è un interfaccia utilizzata in Unity per definire coroutine, che sono funzioni che possono essere eseguite in modo asincrono. Le coroutine consentono di eseguire azioni come attese o operazioni asincrone senza bloccare il flusso principale del programma.
+    IEnumerator FollowPath()
     {
         foreach(Waypoint waypoint in path)
-        {
-            Debug.Log(waypoint.name);
+        {   
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            endPosition.y += 1f;
+            float traverlPercent = 0f;
+
+            while(traverlPercent < 1f){
+            traverlPercent += Time.deltaTime;
+            //lerp si spiega da solo
+            transform.position = Vector3.Lerp(startPosition, endPosition, traverlPercent);
+            yield return new WaitForEndOfFrame(); //letteralmente
+            }
+            
+            //Arrivati a questo punto gli diciamo ritorna qui fra un secondo
+            //yield return new WaitForSeconds(waitTime);
         }
     }
 }

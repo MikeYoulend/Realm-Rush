462b3ad0f6f8555a9cb09a20e97e5fb139c8b0be
MikeYoulend <mikeyoulend@gmail.com>
2024-02-25 11:51:50 +0100

    Hit Points

--- Assets/EnemyHealth.cs
+++ Assets/EnemyHealth.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {      
     [SerializeField] int maxHitPoints = 5;
-    int currentHitPoints = 0;
+    [SerializeField] int currentHitPoints = 0;
     // Start is called before the first frame update
     void Start()
     {

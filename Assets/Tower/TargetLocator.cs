112bc235b0d5ad8eff56441c1f8c9e7524cc8813
MikeYoulend <mikeyoulend@gmail.com>
2024-02-28 15:08:41 +0100

    FindClosestTarget method

--- Assets/Tower/TargetLocator.cs
+++ Assets/Tower/TargetLocator.cs
@@ -6,15 +6,36 @@ public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
     Transform target;
-    void Start()
-    {
-        //Cannot implicitly convert type 'EnemyMover' to 'UnityEngine.Transform' / per questo errore .transform
-        target = FindObjectOfType<Enemy>().transform;
-    }
-
+   
     void Update()
     {
+        FindClosestTarget();
         AimWeapon();
+    }
+
+    void FindClosestTarget()
+    {
+        //array di nemici che verranno trovati by type
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        //capire qual è il nemico più vicino. settato su null perchè ancora non sappiamo chi sarà quello più vicino
+        Transform closestTarget = null;
+        //la distanza di detecting del nemico dalla torre
+        float maxDistance = Mathf.Infinity;
+
+        foreach(Enemy enemy in enemies)
+        {
+            //(la nostra posizione, la posizione del nemico)
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+
+            //come capire quale nemico è più vicino
+            if(targetDistance < maxDistance)
+            {   
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+
+        target = closestTarget;
     }
 
     void AimWeapon()

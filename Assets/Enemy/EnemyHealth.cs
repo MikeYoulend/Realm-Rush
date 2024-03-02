fcbeddc00383a0d0adafd6b10fb01fb1c2e400d9
MikeYoulend <mikeyoulend@gmail.com>
2024-03-02 15:50:22 +0100

    RequireComponent

--- Assets/Enemy/EnemyHealth.cs
+++ Assets/Enemy/EnemyHealth.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//è un attributo che assicura che il componente 
+//specificato sia sempre presente sull'oggetto a cui è applicato lo script. 
+//In questo caso, assicura che l'oggetto abbia sempre un componente Enemy allegato ad esso.
+[RequireComponent(typeof(Enemy))]
 public class EnemyHealth : MonoBehaviour
 {      
     [SerializeField] int maxHitPoints = 5;
-    [SerializeField] int currentHitPoints = 0;
+    [SerializeField] int difficultyRamp = 1;
+    int currentHitPoints = 0;
     
     Enemy enemy;
 
@@ -29,6 +34,8 @@ public class EnemyHealth : MonoBehaviour
         if (currentHitPoints < 1)
         {
             gameObject.SetActive(false);
+            //così aumentiamo la vita dei nemici
+            maxHitPoints += difficultyRamp;
             enemy.RewardGold();
         }
     }

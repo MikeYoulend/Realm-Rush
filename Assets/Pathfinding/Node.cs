7be30c12143cdfdb9727133e80e6ab0b8b9b54da
MikeYoulend <mikeyoulend@gmail.com>
2024-03-04 15:38:26 +0100

    Class Node Data

--- Assets/Pathfinding/Node.cs
+++ Assets/Pathfinding/Node.cs
@@ -2,7 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//NO MonoBehaviour = non è un componente Unity
+//è un semplice classe che contiene dati o logica
 public class Node
 {
-   
+   public Vector2Int coordinates;
+   public bool isWalkable;
+   public bool isExplored;
+   public bool isPath;
+   public Node connectedTo;
+
+
+    //costruttore
+   public Node(Vector2Int coordinates, bool isWalkable)
+   {    
+        //this prende il coordinates che fa parte del constructor
+        this.coordinates = coordinates;
+        this.isWalkable = isWalkable;
+   }
 }

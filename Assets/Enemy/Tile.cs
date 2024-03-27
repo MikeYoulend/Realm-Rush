76cd08cb5a2a726a1ccb0ea8fb4e490d47fc3ff9
MikeYoulend <mikeyoulend@gmail.com>
2024-03-27 18:40:30 +0100

    Valid Path

--- Assets/Enemy/Tile.cs
+++ Assets/Enemy/Tile.cs
@@ -12,11 +12,13 @@ public class Tile : MonoBehaviour
     public bool IsPlaceable { get {return isPlaceable;}}
 
     GridManager gridManager;
+    Pathfinder pathfinder;
     Vector2Int coordinates = new Vector2Int();
 
     void Awake()
     {
         gridManager = FindObjectOfType<GridManager>();
+        pathfinder = FindObjectOfType<Pathfinder>();
     }
 
     void Start() 
@@ -37,15 +39,13 @@ public class Tile : MonoBehaviour
         //Oppure molto semplicemente usiamo il metodo OnMouseDown
         //GetKeyDown quando si clicca una singola volta
         //if(Input.GetMouseButtonDown(0))
-        if(isPlaceable)
+        if(gridManager.GetNode(coordinates).isWalkable && !pathfinder.WillBlockPath(coordinates))
         {   
             //d
             bool isPlaced = towerPrefab.CreateTower(towerPrefab, transform.position);
             //Instantiate(catapult, transform.position, Quaternion.identity);
             isPlaceable = !isPlaced;
+            gridManager.BlockNode(coordinates);
         }
-          
-        
-        
     }
 }
--- Assets/Pathfinding/GridManager.cs
+++ Assets/Pathfinding/GridManager.cs
@@ -42,6 +42,16 @@ public class GridManager : MonoBehaviour
         }
     }
 
+    public void ResetNodes()
+    {
+        foreach(KeyValuePair<Vector2Int, Node> entry in grid)
+        {
+            entry.Value.connectedTo = null;
+            entry.Value.isExplored = false;
+            entry.Value.isPath = false;
+        }
+    }
+
     public Vector2Int GetCoordinateFromPosition(Vector3 position)
     {
         Vector2Int coordinates = new Vector2Int();
